18bf0ff9998da8e7436d4378982a551b2fdfb2ae
Jamaalii <wahid.smkn10@gmail.com>
2020-04-28 20:31:47 +0700

    Update main.go

--- main.go
+++ main.go
@@ -3,20 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	a := [20]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21}
+	a := [20]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	var genap, ganjil, prima []int
 	for i := 0; i < len(a); i++ {
 		if a[i]%2 == 0 {
-			fmt.Println(a[i], "genap")
+			genap = append(genap, a[i])
 		}
 	}
-	// fmt.Println("---------------------------------------------------------------------")
+	fmt.Printf("Bilangan genap : %+v, jumlah = %d\n", genap, len(genap))
+	fmt.Println("---------------------------------------------------------------------")
 
 	for i := 0; i < len(a); i++ {
 		if a[i]%2 == 1 {
-			fmt.Println(a[i], "ganjil")
+			ganjil = append(ganjil, a[i])
 		}
 	}
-	// fmt.Println("---------------------------------------------------------------------")
+	fmt.Printf("Bilangan ganjil : %+v, jumlah = %d\n", ganjil, len(ganjil))
+	fmt.Println("---------------------------------------------------------------------")
 
 	for bil := 1; bil < len(a); bil++ {
 		i := 0
@@ -26,7 +29,9 @@ func main() {
 			}
 		}
 		if (i == 2) && (bil != 1) {
-			fmt.Println(bil, "Prima")
+			prima = append(prima, bil)
 		}
 	}
+	fmt.Printf("Bilangan prima : %+v, jumlah = %d\n", prima, len(prima))
+	fmt.Println("---------------------------------------------------------------------")
 }
